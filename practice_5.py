a619feee7117beae6dea55baa574d20e735ff666
muhammed jalal <muhammedjalal928@gmail.com>
2026-09-18 21:51:52 +0530

    Add two sum practice

---
+++ practice_5.py
@@ -0,0 +1,7 @@
+# Given num and target ,return indicate [i,j](i,j) with num[i]+num[j]=target.and
+# return [-1,-1] if there is None. 10^5 ,so aim for 0 n.
+def two_sum(nums,target):
+    seen={}
+
+    for i,num in enumerate(num):
+        need=
