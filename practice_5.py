b50aae4ca2d576a2e186e9f45a1449e67a2ab66a
muhammed jalal <muhammedjalal928@gmail.com>
2026-09-23 21:19:21 +0530

    Add valid palindrome practice

--- practice_5.py
+++ practice_5.py
@@ -3,5 +3,9 @@
 def two_sum(nums,target):
     seen={}
 
-    for i,num in enumerate(num):
-        need=
+    for i,num in enumerate(nums):
+        need=target-num
+        if need in seen:
+            return[seen[need],i]
+        seen[num]=i
+        return [-1,-1]
---
+++ practice_6.py
@@ -0,0 +1,22 @@
+def validate_Palidrome(s):
+    left=0
+    right=len(s)-1
+
+    while left<right:
+        if not s[left].isalnum():
+            left+=1
+            continue
+        if not s[right].isalnum():
+            left-=1
+            continue
+
+        if s[left].lower()!=s[right].lower():
+            return False
+
+        left+=1
+        right-=1
+
+        return True
+
+
+print(validate_Palidrome("Malayalam"))
